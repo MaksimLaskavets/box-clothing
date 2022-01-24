d4539e9f4a5943c0b7992a5b900796745af89f25
Maksim Laskavets <Laskovets.max@gmail.com>
2022-01-24 23:52:31 +0300

    items with bg

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+
+
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss"
+
+const HomePage = ()=>{
+	return(
+	<div className="homePage">
+	<Directory/>
+</div>)
+	
+}
+export default HomePage;
