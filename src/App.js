fb976b8826d94688e0a640517fe4648cc9ecd363
Maksim Laskavets <Laskovets.max@gmail.com>
2022-02-14 20:16:15 +0300

    update our App component, sign-in component and sign-up component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -14,40 +14,32 @@ import { checkUserSession } from './redux/user/user.actions';
 
 import './App.css';
 
-class App extends React.Component {
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-	 checkUserSession();
-  }
+function App({ checkUserSession, currentUser }) {
+  useEffect(() => {
+    checkUserSession();
+  }, [checkUserSession]);
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  unsubscribeFromAuth = null;
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() => (this.props.currentUser ? (
-              <Redirect to="/" />
-            ) : (
-              <SignInAndSignUp />
-            ))}
-          />
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() => (currentUser ? (
+            <Redirect to="/" />
+          ) : (
+            <SignInAndSignUp />
+          ))}
+        />
+      </Switch>
+    </div>
+  );
 }
+
 const mapStateToProps = (createStructuredSelector)({
   currentUser: selectCurrentUser,
 });
